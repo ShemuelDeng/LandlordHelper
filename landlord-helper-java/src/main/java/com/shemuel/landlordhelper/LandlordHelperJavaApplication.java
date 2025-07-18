c8a384bb1da5cbf604283e438540869e6890e674
dengshaoxiang <dengshaoxiang@vv.cn>
2025-07-18 17:42:57 +0800

    初始提交

---
+++ landlord-helper-java/src/main/java/com/shemuel/landlordhelper/LandlordHelperJavaApplication.java
@@ -0,0 +1,13 @@
+package com.shemuel.landlordhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandlordHelperJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandlordHelperJavaApplication.class, args);
+    }
+
+}
---
+++ landlord-helper-java/src/test/java/com/shemuel/landlordhelper/LandlordHelperJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shemuel.landlordhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandlordHelperJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
